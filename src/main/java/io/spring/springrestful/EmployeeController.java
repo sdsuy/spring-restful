7f71ea3e9ea428d03631c123e3768c97279ddc21
Santiago Da Silva <sdasilva@outlook.es>
2020-12-04 15:17:59 -0300

    Handling a PUT for different clients

--- src/main/java/io/spring/springrestful/EmployeeController.java
+++ src/main/java/io/spring/springrestful/EmployeeController.java
@@ -63,8 +63,8 @@ class EmployeeController {
 	}
 	
 	@PutMapping("/employees/{id}")
-	Employee replaceEmployee(@RequestBody Employee newEmployee, @PathVariable Long id) {
-		return repository.findById(id)
+	ResponseEntity<?> replaceEmployee(@RequestBody Employee newEmployee, @PathVariable Long id) {
+		Employee updatedEmployee = repository.findById(id)
 				.map(employee -> {
 			employee.setName(newEmployee.getName());
 			employee.setRole(newEmployee.getRole());
@@ -74,6 +74,12 @@ class EmployeeController {
 			newEmployee.setId(id);
 			return repository.save(newEmployee);
 		});
+		
+		EntityModel<Employee> entityModel = assembler.toModel(updatedEmployee);
+		
+		return ResponseEntity
+				.created(entityModel.getRequiredLink(IanaLinkRelations.SELF).toUri())
+				.body(entityModel);
 	}
 	
 	@DeleteMapping("/employees/{id}")
